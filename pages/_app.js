07873301784108fd13b3aa5897d8b0b53817e707
vijayanvj <vjdvnt@gmail.com>
2025-07-06 21:31:49 +0530

    content

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,14 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/globals.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        {/* ✅ Correct FontAwesome CDN with verified integrity */}
         <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
-          integrity="sha512-Avb2QiuDEEvB4bZJYdft2mNjVShBftLdPG8FJ0V7irTLQ8Uo0qcPxh4Plq7G5tGm0rU+1SPhVotteLpBERwTkw=="
           crossOrigin="anonymous"
           referrerPolicy="no-referrer"
         />
