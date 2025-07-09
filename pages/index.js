c7a058447e608951ecf6fef290f6838a87231371
vijayanvj <vjdvnt@gmail.com>
2025-07-09 15:36:31 +0530

    icon added

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head';
 import Header from '@/components/Header';
       <Head>
         {/* Primary Meta Tags */}
-        <title>santhosh</title>
+        <title>RS Gear Products – Industrial Gear & Gearbox Manufacturer in India</title>
         <meta name="title" content="RS Gear Products – Industrial Gear & Gearbox Manufacturer in India" />
         <meta name="description" content="RS Gear Products is a top manufacturer of industrial gearboxes, spur gears, bevel gears, and helical gears in India. Serving engineering, automotive, and heavy industries." />
         <meta name="keywords" content="Gearbox, Coir Machinery, Industrial Equipment, Defibering Machine, Conveyor Roller, Baling Press, RS Engineering" />
@@ -29,7 +29,7 @@ import Header from '@/components/Header';
         <link rel="canonical" href="https://rs-gear-products.vercel.app/" />
 
         {/* Favicon */}
-        <link rel="icon" href="/images/logo.png" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
 export default function Home() {
   return (
